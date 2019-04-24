86383afe0c9fce7a674dd5e3e877360336ff022b
Lucy Suddenly <lucysuddenly@Lucys-Air.local.tld>
2019-04-23 19:25:03 -0700

    added structure of Router and Routes

--- src/App.js
+++ src/App.js
@@ -8,16 +8,30 @@ import FormControl from 'react-bootstrap/FormControl'
 
 function App() {
   return (
-  <Navbar sticky="top" bg="light" className="justify-content-between">
-    <Navbar.Brand href="/">TransFundr</Navbar.Brand>
-    <Form inline >
-      <FormControl type="text" placeholder="Username" className="mr-sm-2" />
-      <FormControl type="password" placeholder="Password" className="mr-sm-2"/>
-      <Button type="submit">Login</Button>
-    </Form>
-  </Navbar>
-
-    
+  <>
+    <Navbar sticky="top" bg="light" className="justify-content-between">
+      <Navbar.Brand href="/">TransFundr</Navbar.Brand>
+      <Form inline >
+        <FormControl type="text" placeholder="Username" className="mr-sm-2" />
+        <FormControl type="password" placeholder="Password" className="mr-sm-2"/>
+        <Button type="submit">Login</Button>
+      <Navbar.Text>
+        <a href="/signup">Or Sign Up</a>
+      </Navbar.Text>
+      </Form>
+    </Navbar>
+    <Router>
+      <Switch>
+        <Route exact path="/"/>
+        <Route exact path="/signup"/>
+        <Route exact path="/users/:id"/>
+        <Route exact path="/beacons/:id"/>
+        <Route exact path="/donations/:id"/>
+        <Route exact path="/newdonation"/>
+        <Route exact path="/newbeacon"/>
+      </Switch>
+    </Router>
+ </>
   );
 }
 
