a136df5feb3cb85ccc4a486aff373399ae126d10
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-24 17:46:43 -0700

    added controlled signup form, connected submit of signup to backend user creation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar'
@@ -9,34 +9,49 @@ import Home from './components/home'
 import Signup from './components/signup'
 import NewDonation from './components/newdonation'
 
-function App() {
-  return (
-  <>
-    <Navbar sticky="top" bg="light" className="justify-content-between">
-      <Navbar.Brand href="/">TransFundr</Navbar.Brand>
-      <Form inline >
-        <FormControl type="text" placeholder="Username" className="mr-sm-2" />
-        <FormControl type="password" placeholder="Password" className="mr-sm-2"/>
-        <Button type="submit">Login</Button>
-      <Navbar.Text>
-        <a href="/signup">Or Sign Up</a>
-      </Navbar.Text>
-      </Form>
-    </Navbar>
-    <Router>
-      <Switch>
-        <Route exact path="/" render={(props)=>(<Home {...props}/>)}/>
-        <Route exact path="/signup" render={()=>(<Signup/>)}/>
-        <Route exact path="/users/:id"/>
-        <Route exact path="/beacons/:id"/>
-        <Route exact path="/donations/:id"/>
-        <Route exact path="/newdonation" render={()=>(<NewDonation/>)}/>
-        <Route exact path="/newbeacon"/>
-        <Route exact path="/rankings"/>
-      </Switch>
-    </Router>
- </>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      user: null
+    }
+  }
+
+  logUserIn = (user) => {
+    this.setState({
+      user: user
+    })
+  }
+
+  render(){
+    return (
+      <>
+        <Navbar sticky="top" bg="light" className="justify-content-between">
+          <Navbar.Brand href="/">TransFundr</Navbar.Brand>
+          <Form inline >
+            <FormControl type="text" placeholder="Username" className="mr-sm-2" />
+            <FormControl type="password" placeholder="Password" className="mr-sm-2"/>
+            <Button type="submit">Login</Button>
+          <Navbar.Text>
+            <a href="/signup">Or Sign Up</a>
+          </Navbar.Text>
+          </Form>
+        </Navbar>
+        <Router>
+          <Switch>
+            <Route exact path="/" render={(props)=>(<Home {...this.props}/>)}/>
+            <Route exact path="/signup" render={()=>(<Signup {...this.props} logUserIn={this.logUserIn}/>)}/>
+            <Route exact path="/users/:id"/>
+            <Route exact path="/beacons/:id"/>
+            <Route exact path="/donations/:id"/>
+            <Route exact path="/newdonation" render={()=>(<NewDonation/>)}/>
+            <Route exact path="/newbeacon"/>
+            <Route exact path="/rankings"/>
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/signup.js
+++ src/components/signup.js
@@ -4,6 +4,30 @@ import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 
 class Signup extends Component {
+    constructor(){
+        super()
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+            trans: false,
+            femme: false,
+            nonwhite: false
+        }
+    }
+    onCheckChange = (ev) => {
+        let checkName 
+        checkName = ev.target.name
+        this.setState({
+            [checkName]: !this.state.checkName
+        })
+    }
+
+    onTextFormChange = (ev) => {
+        this.setState({
+            [ev.target.name]: ev.target.value
+        })
+    }
 
     showTransOptions = () => {
         let transOptions
@@ -13,25 +37,39 @@ class Signup extends Component {
         transOptions.classList.add("hidden")
     }
 
+    submitForm = (ev) => {
+        ev.preventDefault()
+        fetch("//localhost:3000/users", {
+          method: "POST",
+          headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify(this.state)
+        })
+        .then(resp => resp.json())
+        .then(json => this.props.logUserIn(json))
+    }
+
     render() {
         return (
             <div class="standaloneform">
-            <Form>
-                <Form.Check value="trans" label="Are you trans?" onClick={this.showTransOptions}/>
+            <Form onSubmit={(ev)=> this.submitForm(ev)}>
+                <Form.Check name="trans" value="trans" label="Are you trans?" onChange={(ev) => this.onCheckChange(ev)} onClick={this.showTransOptions}/>
                 <div id="transoptions" className="hidden">
-                <Form.Check value="femme" label="Are you transfeminine?"/>
-                <Form.Check value="nonwhite" label="Are you non-white?"/>
+                <Form.Check name="femme" value="femme" label="Are you transfeminine?" onChange={(ev) => this.onCheckChange(ev)}/>
+                <Form.Check name="nonwhite" value="nonwhite" label="Are you non-white?" onChange={(ev) => this.onCheckChange(ev)}/>
                 </div>
                 <br/>
                 <Form.Label>Username</Form.Label>
-                <FormControl type="text" placeholder="Enter Username"/>
+                <FormControl value={this.state.username} onChange={(ev)=>{this.onTextFormChange(ev)}} name="username" type="text" placeholder="Enter Username"/>
                 <Form.Label>Email address</Form.Label>
-                <FormControl type="text" placeholder="Enter Email"/>
+                <FormControl value={this.state.email} onChange={(ev)=>{this.onTextFormChange(ev)}} name="email" type="text" placeholder="Enter Email"/>
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                 </Form.Text>
                 <Form.Label>Password</Form.Label>
-                <FormControl type="password" placeholder="Password"/>
+                <FormControl value={this.state.password} onChange={(ev)=>{this.onTextFormChange(ev)}} name="password" type="password" placeholder="Password"/>
                 <Button variant="primary" type="submit">
                     Sign Up
                 </Button>
