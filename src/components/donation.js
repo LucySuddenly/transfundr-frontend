3b18b4d8d1d68d571368107ae9c6f5819f85697d
Lucy Suddenly <suddenlylucy@gmail.com>
2019-04-27 14:21:47 -0700

    added donation container and donations to beacon show page

---
+++ src/components/donation.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import SmProfilePicture from './smprofilepicture'
+import Username from './username'
+import ConfirmedStamp from './confirmedstamp'
+import PendingStamp from './pendingstamp'
+import Button from 'react-bootstrap/Button'
+
+class Donation extends Component {
+    render() {
+        return (
+            <div className="standaloneform">
+                <h2>Donation</h2>
+                { this.props.beacon ? 
+                <h3>{this.props.beacon.title}</h3> : 
+                null }
+                <h5>Sender</h5><SmProfilePicture profile={this.props.donation.user.profile}/><Username user={this.props.donation.user}/>
+                <h5>Receiver</h5><SmProfilePicture profile={this.props.beacon.user.profile}/><Username user={this.props.beacon.user}/>
+                <h2>${this.props.donation.amount} - {this.props.donation.points} Points</h2>
+                { this.props.donation.confirmed ?
+                <ConfirmedStamp/> :
+                <PendingStamp/> 
+                }
+                { this.props.decodeJwt(localStorage.getItem("jwt")) === this.props.beacon.user.id && !this.props.donation.confirmed ?
+                  <Button onClick={this.confirmDonation}>Confirm?</Button> :
+                  null }
+
+                
+            </div>
+        );
+    }
+}
+
+export default Donation;
