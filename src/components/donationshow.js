e80dbb0eb05c80af739e3817d8e63f8cd8789ac5
Lucy Suddenly <lucysuddenly@gmail.com>
2019-04-29 10:46:19 -0700

    added timeago to beacons, donations, and user pages

--- src/components/donationshow.js
+++ src/components/donationshow.js
@@ -5,6 +5,7 @@ import {withRouter} from 'react-router';
 import ConfirmedStamp from './confirmedstamp'
 import PendingStamp from './pendingstamp'
 import Button from 'react-bootstrap/Button'
+import TimeAgo from 'react-timeago'
 
 class DonationShow extends Component {
     constructor(props){
@@ -58,6 +59,7 @@ class DonationShow extends Component {
                 <h5>Sender</h5><SmProfilePicture profile={this.state.donation.user.profile}/><Username user={this.state.donation.user}/>
                 <h5>Receiver</h5><SmProfilePicture profile={this.state.donation.beacon.user.profile}/><Username user={this.state.donation.beacon.user}/>
                 <h2>${this.state.donation.amount} - {this.state.donation.points} Points</h2>
+                <h6>Sent: <TimeAgo date={this.state.donation.created_at}/></h6>
                 { this.state.donation.confirmed ?
                 <ConfirmedStamp/> :
                 <PendingStamp/> 
