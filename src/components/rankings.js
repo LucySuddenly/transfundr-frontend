cc70449d243a888f5cf68ca44fc17f9787e420b9
Lucy Suddenly <suddenlylucy@gmail.com>
2019-05-01 10:35:11 -0700

    styling rankings page and user profiles

--- src/components/rankings.js
+++ src/components/rankings.js
@@ -51,23 +51,25 @@ class Rankings extends Component {
     }
     render() {
         return (
-            <div id="rankings">
-                <div id="topten">
-                    <h2>Top Ten Donators</h2>
-                    <ol>
-                        {this.state.payload.topTen.map(item => {
-                            return <li>{item.points} points - <SmProfilePicture profile={item.user.profile}/><Username user={item.user}/></li>
-                        })}
-                    </ol>
-                </div>
-                <div id="biggestdonations">
-                    <h2>Biggest Donations</h2>
-                    <ul>
-                        <li><a href={`/donations/${this.state.payload.biggestToday.id}`}>Today: ${this.state.payload.biggestToday.amount}</a> - <SmProfilePicture profile={this.state.payload.biggestToday.user.profile}/><Username user={this.state.payload.biggestToday.user}/></li>
-                        <li><a href={`/donations/${this.state.payload.biggestWeek.id}`}>This Week: ${this.state.payload.biggestWeek.amount}</a> - <SmProfilePicture profile={this.state.payload.biggestWeek.user.profile}/><Username user={this.state.payload.biggestWeek.user}/></li>
-                        <li><a href={`/donations/${this.state.payload.biggestMonth.id}`}>This Month: ${this.state.payload.biggestMonth.amount}</a> - <SmProfilePicture profile={this.state.payload.biggestMonth.user.profile}/><Username user={this.state.payload.biggestMonth.user}/></li>
-                        <li><a href={`/donations/${this.state.payload.biggestYear.id}`}>This Year: ${this.state.payload.biggestYear.amount}</a> - <SmProfilePicture profile={this.state.payload.biggestYear.user.profile}/><Username user={this.state.payload.biggestYear.user}/></li>
-                    </ul>
+            <div className="content">
+                <div class="standaloneform">
+                    <div id="topten">
+                        <h2>Top Ten Donators</h2>
+                        <ol>
+                            {this.state.payload.topTen.map(item => {
+                                return <li>{item.points} points - <SmProfilePicture profile={item.user.profile}/><Username user={item.user}/></li>
+                            })}
+                        </ol>
+                    </div>
+                    <div id="biggestdonations">
+                        <h2>Biggest Donations</h2>
+                        <ul>
+                            <li><a href={`/donations/${this.state.payload.biggestToday.id}`}>Today: ${this.state.payload.biggestToday.amount}</a> - <SmProfilePicture profile={this.state.payload.biggestToday.user.profile}/><Username user={this.state.payload.biggestToday.user}/></li>
+                            <li><a href={`/donations/${this.state.payload.biggestWeek.id}`}>This Week: ${this.state.payload.biggestWeek.amount}</a> - <SmProfilePicture profile={this.state.payload.biggestWeek.user.profile}/><Username user={this.state.payload.biggestWeek.user}/></li>
+                            <li><a href={`/donations/${this.state.payload.biggestMonth.id}`}>This Month: ${this.state.payload.biggestMonth.amount}</a> - <SmProfilePicture profile={this.state.payload.biggestMonth.user.profile}/><Username user={this.state.payload.biggestMonth.user}/></li>
+                            <li><a href={`/donations/${this.state.payload.biggestYear.id}`}>This Year: ${this.state.payload.biggestYear.amount}</a> - <SmProfilePicture profile={this.state.payload.biggestYear.user.profile}/><Username user={this.state.payload.biggestYear.user}/></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
--- src/components/transprofile.js
+++ src/components/transprofile.js
@@ -7,20 +7,33 @@ class TransProfile extends Component {
     render() {
         return (
             <>
-            <div class="profile">
-                <img src={this.props.user.profile.cover_img} id="cover_img" alt="cover"/>
-                <div class="imagecropper">
-                <img src={this.props.user.profile.profile_img} className="profile_img" alt="profile"/>
-                </div>
-                <div class="bio">
-                <h6>Bio: {this.props.user.profile.bio}</h6>
-                <h6>Points: {this.props.points}</h6>
-                <h6>Joined: <TimeAgo date={this.props.user.created_at}/></h6>
-                <PaymentContainer profile={this.props.user.profile}/>
-                </div>
-            </div>
+            <img src={this.props.user.profile.cover_img} id="cover_img" alt="cover"/>
             <div className="content">
-            <BeaconContainer payload={this.props.beacons}/>
+                <div className="profile">
+                    <div>
+                        <div className="inline align-middle">
+                            <div className="imagecropper">
+                                <img src={this.props.user.profile.profile_img} className="profile_img" alt="profile"/>
+                            </div>
+                        </div>
+                        <div className="divider"/>
+                        <h1 class="inline align-middle">{this.props.user.username}</h1>
+                        <div className="inline align-middle flag">
+                            <div className="imagecropper">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Transgender_Pride_flag.svg/2560px-Transgender_Pride_flag.svg.png"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bio">
+                        <div className="inline">
+                            <h5>Bio: {this.props.user.profile.bio}</h5>
+                            <h3>Points: {this.props.points}</h3>
+                            <h6>Joined: <TimeAgo date={this.props.user.created_at}/></h6>
+                        </div>
+                    <PaymentContainer profile={this.props.user.profile}/>
+                    </div>
+                </div>
+                <BeaconContainer payload={this.props.beacons}/>
             </div>
             </>
         );
