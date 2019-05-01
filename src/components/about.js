424852048b039f31ef569af3bd957cd6e903c841
Lucy Suddenly <suddenlylucy@gmail.com>
2019-05-01 11:24:54 -0700

    updated contact email

--- src/components/about.js
+++ src/components/about.js
@@ -27,7 +27,7 @@ class About  extends Component {
                     <br/>
                     <p>Trans women are at high risk of joblessness, homelessness, assault, and societal exclusion. However, trans men of color also experience high incidences of these ills. And of course, the risks are especially compounded for trans women of color. </p>
                     <p>Allies: the way to best use this site is to donate when you can and share beacons as often as you’re able. Promoting fundraisers of trans people is almost as good as donating. Call on your friends and family to use this site. Trans people are in need, and you can help.</p>
-                    <p>Trans folks: this website serves as the opening in a conversation. I made design decisions based on my knowledge of hierarchy in our community, but these decisions are flexible: if you have ideas on how I can better structure the economy of points or anything else, <a href="mailto:suddenlylucy@gmail.com">email me</a> -- if you can code, head to the <a href="https://github.com/LucySuddenly/transfundr-frontend">git repo</a> and submit a pull request.</p>
+                    <p>Trans folks: this website serves as the opening in a conversation. I made design decisions based on my knowledge of hierarchy in our community, but these decisions are flexible: if you have ideas on how I can better structure the economy of points or anything else, <a href="mailto:transfundr@gmail.com">email me</a> -- if you can code, head to the <a href="https://github.com/LucySuddenly/transfundr-frontend">git repo</a> and submit a pull request.</p>
                 </div>
             </div>
         );
