32e40b477af6befc4b623674b63ea6bd7e841d49
Lucy Suddenly <suddenlylucy@gmail.com>
2020-01-09 10:01:59 -0800

    Updates

---
+++ precache-manifest.af3029378d609f3671d0a91bca54c222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fdaa357437965f33e6eb000811b41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae0b665322bb5688206b",
+    "url": "/static/css/2.dc646829.chunk.css"
+  },
+  {
+    "revision": "1828aa0ccac13738cb9b",
+    "url": "/static/css/main.2c8fdb8a.chunk.css"
+  },
+  {
+    "revision": "ae0b665322bb5688206b",
+    "url": "/static/js/2.b17b7f96.chunk.js"
+  },
+  {
+    "revision": "1828aa0ccac13738cb9b",
+    "url": "/static/js/main.bd1ea679.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c614347ab1c7c0ae96b267c57f666aef",
+    "url": "/static/media/full-bloom.c614347a.png"
+  }
+]);
