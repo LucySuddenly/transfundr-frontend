a8f9bc48dbccbc176a58d096d91981f3b2b26d0a
Lucy Suddenly <suddenlylucy@gmail.com>
2020-01-15 12:24:57 -0800

    improvements to ally profile

--- src/components/allyprofile.js
+++ src/components/allyprofile.js
@@ -6,40 +6,38 @@ import Button from 'react-bootstrap/Button'
 class AllyProfile extends Component {
     render() {
         return (
-             <>
-             <img src={this.props.user.profile.cover_img} id="cover_img" alt="cover"/>
-             <div className="content">
-                 <div className="profile">
-                     <div>
-                         <div className="inline align-middle">
-                             <div className="imagecropper">
-                                 <img src={this.props.user.profile.profile_img} className="profile_img" alt="profile"/>
-                             </div>
-                         </div>
-                         <div className="divider"/>
-                         <h1 class="inline align-middle">{this.props.user.username}</h1>
-                         <div className="inline align-middle flag">
-                             <div className="imagecropper">
-                                 <img className="profile_img" src="../../images/Ally.png" alt="ally flag"/>
-                             </div>
-                         </div>
-                     </div>
-                     <div class="bio">
-                         <div className="inline">
-                            {localStorage.getItem("jwt") && this.props.decodeJwt(localStorage.getItem("jwt")) === this.props.user.id ?
-                             <Button href="/#/editprofile">Edit Profile</Button>
-                            : 
-                             null
-                            }
-                             <h5 className="text">Bio: {this.props.user.profile.bio}</h5>
-                             <h3>Points: {this.props.points}</h3>
-                             <h6>Joined: <TimeAgo date={this.props.user.created_at}/></h6>
-                         </div>
-                     </div>
-                 </div>
-                 <DonationContainer decodeJwt={this.props.decodeJwt} donations={this.props.user.donations} />
-             </div>
-             </>
+            <>
+                <img src={this.props.user.profile.cover_img} id="cover_img" alt="cover"/>
+                <div className="content">
+                    <div className="profile">
+                        <div className="inline align-middle">
+                            <div className="imagecropper">
+                                <img src={this.props.user.profile.profile_img} className="profile_img" alt="profile"/>
+                            </div>
+                        </div>
+                        <div className="divider"/>
+                        <h1 class="inline align-middle">{this.props.user.username}</h1>
+                        <div className="inline align-middle flag">
+                            <div className="imagecropper">
+                                <img className="profile_img" src="../../images/Ally.png" alt="ally flag"/>
+                            </div>
+                        </div>
+                        <div class="bio">
+                            <div className="inline">
+                                {localStorage.getItem("jwt") && this.props.decodeJwt(localStorage.getItem("jwt")) === this.props.user.id ?
+                                <Button href="/#/editprofile">Edit Profile</Button>
+                                : 
+                                null
+                                }
+                                <h5 className="text">Bio: {this.props.user.profile.bio}</h5>
+                                <h3>Points: {this.props.points}</h3>
+                                <h6>Joined: <TimeAgo date={this.props.user.created_at}/></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <DonationContainer decodeJwt={this.props.decodeJwt} donations={this.props.user.donations} />
+                </div>
+            </>
 
         );
     }
